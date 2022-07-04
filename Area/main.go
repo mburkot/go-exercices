df6e811c16016462a8a7321df5c268f2c3fd9bc2
Matheus Burkot <m.c.burkot@gmail.com>
2022-07-04 20:10:14 -0300

    More excercices

---
+++ Area/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type square struct {
+	sideLength float64
+}
+
+type triangle struct {
+	height float64
+	base   float64
+}
+
+type shape interface {
+	getArea() float64
+}
+
+func main() {
+
+	s := square{
+		sideLength: 4,
+	}
+
+	t := triangle{
+		height: 3,
+		base:   4,
+	}
+
+	printArea(s)
+	printArea(t)
+
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return t.height * t.base * 0.5
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
